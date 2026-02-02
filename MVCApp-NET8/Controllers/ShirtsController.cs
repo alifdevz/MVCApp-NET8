09990fab21e6184ff4a534399aba63ec5346091f
alifdevz <alifm.devz@gmail.com>
2026-02-02 20:17:47 +0800

    Extends ShirtsController to ControllerBase by decorating with [ApiController] first

---
+++ MVCApp-NET8/Controllers/ShirtsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApp_NET8.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")] // [controller] represents shirts
+    public class ShirtsController : ControllerBase
+    {
+        
+
+    }
+}
