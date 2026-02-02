6b6a05d59526f22880db0101f298b5d8ef3c340c
alifdevz <alifm.devz@gmail.com>
2026-02-02 20:30:17 +0800

    Create CRUD operations that just return strings

--- MVCApp-NET8/Controllers/ShirtsController.cs
+++ MVCApp-NET8/Controllers/ShirtsController.cs
@@ -6,7 +6,34 @@ namespace MVCApp_NET8.Controllers
     [Route("/api/[controller]")] // [controller] represents shirts
     public class ShirtsController : ControllerBase
     {
-        
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
 
+        [HttpGet("{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Reading shirt with ID: {id}";
+        }
+
+        [HttpPost]
+        public string CreateShirt()
+        {
+            return "Creating a new shirt";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt with ID: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting shirt with ID: {id}";
+        }
     }
 }
